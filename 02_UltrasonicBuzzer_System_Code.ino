1090bc634498699c43b6cb030f5ae0ca4fb025b5
Naveen K <129737291+arkNaveen@users.noreply.github.com>
2023-05-23 10:30:42 +0530

    With a PIR sensor for verification

--- 02_UltrasonicBuzzer_System_Code.ino
+++ 02_UltrasonicBuzzer_System_Code.ino
@@ -3,12 +3,14 @@ int trig = 7;
 int buzz = 13; 
 int time;
 int distance; 
+int ir_pin=2;
 void setup() 
 {
   Serial.begin (9600); 
   pinMode (trig, OUTPUT); 
   pinMode (echo, INPUT);
   pinMode (buzz, OUTPUT);
+  pinMode (ir_pin, INPUT);
 }
 void loop() 
 {
@@ -23,6 +25,8 @@ void loop()
         Serial.print (" Distance= ");              
         Serial.println (distance);        
         digitalWrite (buzz, HIGH);
+    	if(digitalRead(2)==LOW)
+          Serial.print("Motion detected by IR sensor");  // if the object is too close proximity sensor gets activated 
         delay (500);
         }
   else {
